f463209c3d7ef2bfb7c99c1fc4b58f71529f0e85
mike <mike@C46002.csd.local>
2025-10-13 10:34:32 -0400

    Fix: re-add useEffect import needed by EntityNetworkBuilder

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { SignalIcon } from './components/SignalIcon';
 import EntityNetworkD3 from './components/EntityNetworkD3';
 import EntityDetailsPanel, { EntityData } from './components/EntityDetailsPanel';
